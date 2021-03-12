207d4b56fe043231b9dab19061dadcf5a5402d44
sergTesst <sergiycheck@gmail.com>
2021-03-12 21:28:45 +0200

    add explore template

---
+++ src/components/explore/Explore.jsx
@@ -0,0 +1,286 @@
+import React from 'react';
+
+import './Explore.scss';
+
+class Explore extends React.Component{
+
+	render(){
+		return(
+
+<div className="site-wrap">
+
+
+<header
+	className="header-bar d-flex d-lg-block
+		align-items-center
+		aos-init aos-animate"
+		data-aos="fade-left">
+
+	<div className="main-menu">
+		<ul className="">
+			<li className="active"><a href="/explore">all posts</a></li>
+			<li><a href="/explore">photos</a></li>
+			<li><a href="/explore">videos</a></li>
+			<li><a href="/explore">people</a></li>
+			<li><a href="/explore">groups</a></li>
+			<li><a href="/explore">channels</a></li>
+			<li><a href="/explore">live</a></li>
+			</ul>
+	</div>
+
+</header>
+
+
+<div className="main-content">
+
+	<div className="container-fluid photos">
+
+		<div className="row align-items-stretch my-4 p-2">
+
+			<div className="col-6 col-md-6 col-lg-8  aos-init aos-animate p-1" data-aos="fade-up">
+				<a href="/explore" className="d-block photo-item">
+				<img src="./assets/img/explore_imgs/img_4.jpg" alt="Post" className="img-fluid"/>
+
+				<div className="photo-text-more">
+					<div className="photo-text-more">
+						<h3 className="heading">Photos Title Here</h3>
+						<span className="meta">42 Photos</span>
+					</div>
+				</div>
+				</a>
+
+			</div>
+
+			<div className="col-6 col-md-6 col-lg-4  aos-init aos-animate p-1" data-aos="fade-up" data-aos-delay="100">
+				<a href="/explore" className="d-block photo-item">
+				<img src="./assets/img/explore_imgs/img_5.jpg" alt="Post" className="img-fluid"/>
+
+					<div className="photo-text-more">
+						<div className="photo-text-more">
+							<h3 className="heading">Photos Title Here</h3>
+							<span className="meta">42 Photos</span>
+						</div>
+					</div>
+				</a>
+			</div>
+
+			<div className="col-6 col-md-6 col-lg-3  aos-init aos-animate p-1" data-aos="fade-up">
+			<a href="/explore" className="d-block photo-item">
+			<img src="./assets/img/explore_imgs/img_1.jpg" alt="Post" className="img-fluid"/>
+			<div className="photo-text-more">
+			<h3 className="heading">Photos Title Here</h3>
+			<span className="meta">42 Photos</span>
+			</div>
+			</a>
+			</div>
+
+			<div className="col-6 col-md-6 col-lg-6  aos-init aos-animate p-1" data-aos="fade-up" data-aos-delay="200">
+			<a href="/explore" className="d-block photo-item">
+			<img src="./assets/img/explore_imgs/img_2.jpg" alt="Post" className="img-fluid"/>
+			<div className="photo-text-more">
+			<div className="photo-text-more">
+			<h3 className="heading">Photos Title Here</h3>
+			<span className="meta">42 Photos</span>
+			</div>
+			</div>
+			</a>
+			</div>
+
+			<div className="col-6 col-md-6 col-lg-3  aos-init aos-animate p-1" data-aos="fade-up" data-aos-delay="300">
+			<a href="/explore" className="d-block photo-item">
+			<img src="./assets/img/explore_imgs/img_3.jpg" alt="Post" className="img-fluid"/>
+			<div className="photo-text-more">
+			<div className="photo-text-more">
+			<h3 className="heading">Photos Title Here</h3>
+			<span className="meta">42 Photos</span>
+			</div>
+			</div>
+			</a>
+			</div>
+
+			<div className="col-6 col-md-6 col-lg-6  aos-init aos-animate p-1" data-aos="fade-up" data-aos-delay="">
+			<a href="/explore" className="d-block photo-item">
+			<img src="./assets/img/explore_imgs/img_6.jpg" alt="Post" className="img-fluid"/>
+			<div className="photo-text-more">
+			<div className="photo-text-more">
+			<h3 className="heading">Photos Title Here</h3>
+			<span className="meta">42 Photos</span>
+			</div>
+			</div>
+			</a>
+			</div>
+
+			<div className="col-6 col-md-6 col-lg-6  aos-init aos-animate p-1" data-aos="fade-up" data-aos-delay="100">
+			<a href="/explore" className="d-block photo-item">
+			<img src="./assets/img/explore_imgs/img_7.jpg" alt="Post" className="img-fluid"/>
+			<div className="photo-text-more">
+			<div className="photo-text-more">
+			<h3 className="heading">Photos Title Here</h3>
+			<span className="meta">42 Photos</span>
+			</div>
+			</div>
+			</a>
+			</div>
+
+			<div className="col-6 col-md-6 col-lg-4  aos-init aos-animate p-1" data-aos="fade-up" data-aos-delay="">
+			<a href="/explore" className="d-block photo-item">
+			<img src="./assets/img/explore_imgs/img_8.jpg" alt="Post" className="img-fluid"/>
+			<div className="photo-text-more">
+			<div className="photo-text-more">
+			<h3 className="heading">Photos Title Here</h3>
+			<span className="meta">42 Photos</span>
+			</div>
+			</div>
+			</a>
+			</div>
+
+			<div className="col-6 col-md-6 col-lg-4  aos-init aos-animate p-1" data-aos="fade-up" data-aos-delay="100">
+			<a href="/explore" className="d-block photo-item">
+			<img src="./assets/img/explore_imgs/img_9.jpg" alt="Post" className="img-fluid"/>
+			<div className="photo-text-more">
+			<div className="photo-text-more">
+			<h3 className="heading">Photos Title Here</h3>
+			<span className="meta">42 Photos</span>
+			</div>
+			</div>
+			</a>
+			</div>
+
+			<div className="col-6 col-md-6 col-lg-4  aos-init aos-animate p-1" data-aos="fade-up" data-aos-delay="200">
+			<a href="/explore" className="d-block photo-item">
+			<img src="./assets/img/explore_imgs/img_10.jpg" alt="Post" className="img-fluid"/>
+			<div className="photo-text-more">
+			<div className="photo-text-more">
+			<h3 className="heading">Photos Title Here</h3>
+			<span className="meta">42 Photos</span>
+			</div>
+			</div>
+			</a>
+			</div>
+
+			<div className="col-6 col-md-6 col-lg-3  aos-init aos-animate p-1" data-aos="fade-up" data-aos-delay="">
+			<a href="/explore" className="d-block photo-item">
+			<img src="./assets/img/explore_imgs/img_1.jpg" alt="Post" className="img-fluid"/>
+			<div className="photo-text-more">
+			<div className="photo-text-more">
+			<h3 className="heading">Photos Title Here</h3>
+			<span className="meta">42 Photos</span>
+			</div>
+			</div>
+			</a>
+			</div>
+
+			<div className="col-6 col-md-6 col-lg-6  aos-init aos-animate p-1" data-aos="fade-up" data-aos-delay="100">
+			<a href="/explore" className="d-block photo-item">
+			<img src="./assets/img/explore_imgs/img_2.jpg" alt="Post" className="img-fluid"/>
+			<div className="photo-text-more">
+			<div className="photo-text-more">
+			<h3 className="heading">Photos Title Here</h3>
+			<span className="meta">42 Photos</span>
+			</div>
+			</div>
+			</a>
+			</div>
+
+			<div className="col-6 col-md-6 col-lg-3  aos-init aos-animate p-1" data-aos="fade-up" data-aos-delay="200">
+			<a href="/explore" className="d-block photo-item">
+			<img src="./assets/img/explore_imgs/img_3.jpg" alt="Post" className="img-fluid"/>
+			<div className="photo-text-more">
+			<div className="photo-text-more">
+			<h3 className="heading">Photos Title Here</h3>
+			<span className="meta">42 Photos</span>
+			</div>
+			</div>
+			</a>
+			</div>
+
+			<div className="col-6 col-md-6 col-lg-8  aos-init aos-animate p-1" data-aos="fade-up" data-aos-delay="">
+			<a href="/explore" className="d-block photo-item">
+			<img src="./assets/img/explore_imgs/img_4.jpg" alt="Post" className="img-fluid"/>
+			<div className="photo-text-more">
+			<div className="photo-text-more">
+			<h3 className="heading">Photos Title Here</h3>
+			<span className="meta">42 Photos</span>
+			</div>
+			</div>
+			</a>
+			</div>
+
+			<div className="col-6 col-md-6 col-lg-4  aos-init aos-animate p-1" data-aos="fade-up" data-aos-delay="100">
+			<a href="/explore" className="d-block photo-item">
+			<img src="./assets/img/explore_imgs/img_5.jpg" alt="Post" className="img-fluid"/>
+			<div className="photo-text-more">
+			<div className="photo-text-more">
+			<h3 className="heading">Photos Title Here</h3>
+			<span className="meta">42 Photos</span>
+			</div>
+			</div>
+			</a>
+			</div>
+
+			<div className="col-6 col-md-6 col-lg-6 aos-init  p-1" data-aos="fade-up" data-aos-delay="">
+			<a href="/explore" className="d-block photo-item">
+			<img src="./assets/img/explore_imgs/img_6.jpg" alt="Post" className="img-fluid"/>
+			<div className="photo-text-more">
+			<div className="photo-text-more">
+			<h3 className="heading">Photos Title Here</h3>
+			<span className="meta">42 Photos</span>
+			</div>
+			</div>
+			</a>
+			</div>
+
+			<div className="col-6 col-md-6 col-lg-6 aos-init  p-1" data-aos="fade-up" data-aos-delay="100">
+			<a href="/explore" className="d-block photo-item">
+			<img src="./assets/img/explore_imgs/img_7.jpg" alt="Post" className="img-fluid"/>
+			<div className="photo-text-more">
+			<div className="photo-text-more">
+			<h3 className="heading">Photos Title Here</h3>
+			<span className="meta">42 Photos</span>
+			</div>
+			</div>
+			</a>
+			</div>
+
+			<div className="col-6 col-md-6 col-lg-4 aos-init  p-1" data-aos="fade-up" data-aos-delay="">
+				<a href="/explore" className="d-block photo-item">
+				<img src="./assets/img/explore_imgs/img_8.jpg" alt="Post" className="img-fluid"/>
+				<div className="photo-text-more">
+				<div className="photo-text-more">
+				<h3 className="heading">Photos Title Here</h3>
+				<span className="meta">42 Photos</span>
+				</div>
+				</div>
+				</a>
+			</div>
+
+			<div className="col-6 col-md-6 col-lg-8 aos-init  p-1" data-aos="fade-up" data-aos-delay="100">
+				<a href="/explore" className="d-block photo-item">
+				<img src="./assets/img/explore_imgs/img_9.jpg" alt="Post" className="img-fluid"/>
+				<div className="photo-text-more">
+				<div className="photo-text-more">
+				<h3 className="heading">Photos Title Here</h3>
+				<span className="meta">42 Photos</span>
+				</div>
+				</div>
+				</a>
+			</div>
+
+		</div>
+
+	</div>
+
+</div>
+
+
+</div>
+
+
+
+
+
+		);
+	}
+}
+
+export default Explore;
